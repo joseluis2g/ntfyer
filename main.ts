38cd82e3d5be2f4d7c3c00260531ed0aed155184
Joseluis González <joseluis.ggr@gmail.com>
2024-03-08 18:35:54 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,8 +7,8 @@ import { cron } from "https://deno.land/x/deno_cron@v1.0.0/cron.ts";
 // For parsing args
 import args from "https://deno.land/x/args@2.1.1/index.ts";
 import {
-  EarlyExitFlag,
-  PartialOption,
+	EarlyExitFlag,
+	PartialOption,
 } from "https://deno.land/x/args@2.1.1/flag-types.ts";
 import { Text } from "https://deno.land/x/args@2.1.1/value-types.ts";
 import { MAIN_COMMAND } from "https://deno.land/x/args@2.1.1/symbols.ts";
@@ -20,69 +20,69 @@ import { Maybe } from "./src/maybe.ts";
 
 // Args
 const parser = args
-  .describe("Sending various information to your ntfy.sh topic by time")
-  .with(EarlyExitFlag("help", {
-    alias: ["?"],
-    describe: "Show help",
-    exit() {
-      console.log("USAGE:");
-      console.log("  deno run --allow-net main.ts --topic <text>");
-      console.log(parser.help());
-      return Deno.exit(0);
-    },
-  }))
-  .with(PartialOption("logging", {
-    describe: "Enable logging to directory",
-    type: Text,
-    default: "",
-  }))
-  .with(PartialOption("topic", {
-    describe: "ntfy.sh topic",
-    type: Text,
-    default: "ntfyer-default-topic",
-  }));
+	.describe("Sending various information to your ntfy.sh topic by time")
+	.with(
+		EarlyExitFlag("help", {
+			alias: ["?"],
+			describe: "Show help",
+			exit() {
+				console.log("USAGE:");
+				console.log("  deno run --allow-net main.ts --topic <text>");
+				console.log(parser.help());
+				return Deno.exit(0);
+			},
+		})
+	)
+	.with(
+		PartialOption("logging", {
+			describe: "Enable logging to directory",
+			type: Text,
+			default: "",
+		})
+	);
 
 // Parse args
 const parserRes = parser.parse(Deno.args);
 
 // Check if not main
 if (parserRes.tag !== MAIN_COMMAND) {
-  console.error(parserRes.error.toString());
-  throw Deno.exit(1);
+	console.error(parserRes.error.toString());
+	throw Deno.exit(1);
 }
 
 // Unknown flags (args)
 if (parserRes.remaining().rawFlags().length) {
-  console.error("Unknown flags:", ...parserRes.remaining().rawFlags());
-  throw Deno.exit(1);
+	console.error("Unknown flags:", ...parserRes.remaining().rawFlags());
+	throw Deno.exit(1);
 }
 
 // Get topic value
-const { topic, logging } = parserRes.value;
+const { logging } = parserRes.value;
 
 // Logging
 let logger: Logger | null = null;
 if (logging != "") {
-  logger = new Logger();
-  await logger.initFileLogger(logging, {
-    rotate: true,
-  });
-  logger.disableConsole();
+	logger = new Logger();
+	await logger.initFileLogger(logging, {
+		rotate: true,
+	});
+	logger.disableConsole();
 }
 
 // Create ntfy.sh client
-const client = new NtfyClient(topic);
+const client = new NtfyClient("infernaglobal-probando");
 
 // Setup ntfyer modules
 config.forEach((mod) => {
-  cron(mod.at, () => {
-    mod.module.run().then((text) => {
-      // Write logs
-      logger?.info(mod.title, { text: text });
+	cron(mod.at, () => {
+		mod.module.run().then((text) => {
+			// Write logs
+			logger?.info(mod.title, { text: text });
 
-      // Send message to topic
-      Maybe.from(text)
-        .map((text) => client.send(text as string, { "Title": mod.title }));
-    });
-  });
+			// Send message to topic
+			Maybe.from(text).map((text) =>
+				client.send(text as string, { Title: mod.title })
+			);
+		});
+	});
 });
